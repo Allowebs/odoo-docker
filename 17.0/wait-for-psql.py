f3fdb29c0c7b3bb058f6b0e4cfa90e8e90b0d8ab
Adryan Serage <17680194+KazeroG@users.noreply.github.com>
2024-04-10 09:28:15 -0400

    Update wait-for-psql.py

--- 17.0/wait-for-psql.py
+++ 17.0/wait-for-psql.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('--db_port', required=True)
     arg_parser.add_argument('--db_user', required=True)
     arg_parser.add_argument('--db_password', required=True)
+    arg_parser.add_argument('--db_name', default='postgres')  # Set default database name to 'postgres'
     arg_parser.add_argument('--timeout', type=int, default=5)
 
     args = arg_parser.parse_args()
@@ -18,7 +19,13 @@ if __name__ == '__main__':
     start_time = time.time()
     while (time.time() - start_time) < args.timeout:
         try:
-            conn = psycopg2.connect(user=args.db_user, host=args.db_host, port=args.db_port, password=args.db_password, dbname='postgres')
+            conn = psycopg2.connect(
+                user=args.db_user,
+                password=args.db_password,
+                host=args.db_host,
+                port=args.db_port,
+                dbname=args.db_name  # Use the database name from arguments
+            )
             error = ''
             break
         except psycopg2.OperationalError as e:
@@ -28,5 +35,5 @@ if __name__ == '__main__':
         time.sleep(1)
 
     if error:
-        print("Database connection failure: %s" % error, file=sys.stderr)
+        print(f"Database connection failure: {error}", file=sys.stderr)
         sys.exit(1)
